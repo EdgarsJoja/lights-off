96c81388ccf99e955ce8960d7e1acca29784675f
Edgars Joja <edgarsjoja@gmail.com>
2021-02-21 17:02:38 +0200

    Basic extension setup.

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  configureWebpack: {
+    devtool: 'cheap-module-source-map'
+  },
+  chainWebpack: config => {
+    config.plugin('copy').tap(args => {
+      args[0].push(
+        {
+          from: 'src/manifest.json',
+          to: 'manifest.json'
+        },
+        {
+          from: 'src/sw.js',
+          to: 'sw.js'
+        }
+      );
+      return args;
+    });
+  }
+};
